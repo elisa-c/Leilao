56361721ea65e6e89e03813221ad9b51f3ce2cc1
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-24 00:47:23 -0300

    :white_check_mark: second unit test

--- LeilaoTests/AvaliadorTests.swift
+++ LeilaoTests/AvaliadorTests.swift
@@ -49,4 +49,16 @@ class AvaliadorTests: XCTestCase {
         XCTAssertEqual(400.0, leiloeiro.maiorLance())
 
     }
+    
+    func testDeveEntenderLeilaoComApenasUmLance() {
+        let joao = Usuario(nome: "Joao")
+        let leilao = Leilao(descricao: "Playstation")
+        leilao.propoe(lance: Lance(joao, 1000.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        XCTAssertEqual(1000.0, leiloeiro.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+    }
 }
